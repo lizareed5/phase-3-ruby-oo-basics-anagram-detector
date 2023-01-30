5720f852f7d52e4998d300b4cd2cf48a042d804c
Liza McLain <lizamclain@gmail.com>
2023-01-30 17:44:52 -0600

    commit tree 46a21e7ac880c35357dee61206f78eefbf0aa576

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
 # Your code goes here!
+require 'pry'
+
+class Anagram
+    attr_accessor :word
+
+    def initialize(word)
+        self.word = word
+    end
+
+    def match words
+        words.filter do |word|
+            word.split('').sort == self.word.split('').sort
+        end
+    end
+
+end
